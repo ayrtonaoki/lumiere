1906d33333e52cb1161c95c948859f9cd4531388
ayrtonaoki <ayrtonsasouza@gmail.com>
2024-09-23 18:27:58 -0300

    Creates therapist model, migration and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_21_214337) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_23_210518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_21_214337) do
   end
 
   create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "therapists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
-# Create User
 if User.count == 0
   User.create!(email: ENV['USER_EMAIL'], password: ENV['USER_PASSWORD'], password_confirmation: ENV['USER_PASSWORD'])
 end
 
-# Create Admin
 if Admin.count == 0
   Admin.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'])
 end
 
-# Create Patients
 if Patient.count == 0
   Patient.create!(name: 'José')
   Patient.create!(name: 'Maria')
@@ -16,7 +13,6 @@ if Patient.count == 0
   Patient.create!(name: 'Ana')
 end
 
-# Create Exercises
 if Exercise.count == 0
   Exercise.create!(name: 'Pular')
   Exercise.create!(name: 'Jogar bola')
@@ -24,6 +20,12 @@ if Exercise.count == 0
   Exercise.create!(name: 'Pequeno x Grande')
   Exercise.create!(name: 'Quebra cabeça de 3 peças')
   Exercise.create!(name: 'Esquerda x Direita')
+end
+
+if Therapist.count == 0
+  Therapist.create!(name: 'Jeovana')
+  Therapist.create!(name: 'Rose')
+  Therapist.create!(name: 'Andressa')
 end
 
 if PatientExercise.count == 0
